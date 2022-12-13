4fa9148322116d313ca3095a8226a378af650aff
Evgenia Noskova <evgeniyafelicity.88@gmail.com>
2022-12-13 06:48:01 +0300

    Исправила имена методов

--- Lab5/Main.java
+++ Lab5/Main.java
@@ -4,47 +4,47 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("------------First method---------------");
+        System.out.println("-----------getAverageVal----------------");
         List<Integer> list1 = List.of(1, 2, 3, 4, 5);
-        System.out.println(StreamMethods.method1(list1));
+        System.out.println(StreamMethods.getAverageVal(list1));
         System.out.println();
 
-        System.out.println("------------Second method---------------");
+        System.out.println("------------toUpCase---------------");
         List<String> list2 = List.of("one", "two", "three");
-        System.out.println(StreamMethods.method2(list2));
+        System.out.println(StreamMethods.toUpCase(list2));
         System.out.println();
 
-        System.out.println("------------Third method---------------");
+        System.out.println("-------------getUniqueToSqrt--------------");
         List<Integer> list3 = List.of(1, 2, 2, 3, 4, 4);
-        System.out.println(StreamMethods.method3(list3));
+        System.out.println(StreamMethods.getUniqueToSqrt(list3));
         System.out.println();
 
-        System.out.println("------------Fourth method---------------");
+        System.out.println("------------sortStrStartedWithLetter---------------");
         List<String> list4 = List.of("one", "two", "tree", "four", "five");
-        System.out.println(StreamMethods.method4(list4, "f"));
+        System.out.println(StreamMethods.sortStrStartedWithLetter(list4, "f"));
         System.out.println();
 
-        System.out.println("------------Fifth method---------------");
+        System.out.println("------------getLastElem---------------");
         List<Double> list5 = List.of(1.1, 1.2, 1.3);
         List<Integer> arrayList5 = new ArrayList<>();
         try {
-            System.out.println(StreamMethods.method5(list5));
-            System.out.println(StreamMethods.method5(arrayList5));
+            System.out.println(StreamMethods.getLastElem(list5));
+            System.out.println(StreamMethods.getLastElem(arrayList5));
         }catch (NoSuchElementException e){
             System.out.println(e.getMessage());
         }
         System.out.println();
 
-        System.out.println("------------Sixth method---------------");
+        System.out.println("------------getSumOfEvenNum---------------");
         int[] array61 = {1, 2, 3, 4, 5};
         int[] array62 = {1, 3, 5};
-        System.out.println(StreamMethods.method6(array61));
-        System.out.println(StreamMethods.method6(array62));
+        System.out.println(StreamMethods.getSumOfEvenNum(array61));
+        System.out.println(StreamMethods.getSumOfEvenNum(array62));
         System.out.println();
 
-        System.out.println("------------Seventh method---------------");
+        System.out.println("-------------convertToMapq--------------");
         List<String> list7 = List.of("one", "tree", "four");
-        System.out.println(StreamMethods.method7(list7));
+        System.out.println(StreamMethods.convertToMap(list7));
         System.out.println();
     }
 }
--- Lab5/StreamMethods.java
+++ Lab5/StreamMethods.java
@@ -4,21 +4,21 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class StreamMethods {
-    public static double method1(List<Integer> list){
+    public static double getAverageVal(List<Integer> list){
         return list.stream()
                 .mapToInt(Integer::intValue)
                 .summaryStatistics()
                 .getAverage();
     }
 
-    public static List<String> method2(List<String> list){
+    public static List<String> toUpCase(List<String> list){
         return list.stream()
                 .map(String::toUpperCase)
                 .map(s -> "_new_" + s)
                 .toList();
     }
 
-    public static List<Integer> method3(List<Integer> list){
+    public static List<Integer> getUniqueToSqrt(List<Integer> list){
         return list.stream()
                 .collect(Collectors.groupingBy(x -> x, Collectors.counting()))
                 .entrySet()
@@ -29,26 +29,26 @@ public class StreamMethods {
                 .toList();
     }
 
-    public static List<String> method4(Collection<String> list, String startElement){
+    public static List<String> sortStrStartedWithLetter(Collection<String> list, String startElement){
         return list.stream()
                 .filter(x -> x.startsWith(startElement))
                 .sorted(String.CASE_INSENSITIVE_ORDER)
                 .toList();
     }
 
-    public static <T> T method5(Collection <T> collection){
+    public static <T> T getLastElem(Collection <T> collection){
         return collection.stream()
                 .reduce((x, y) -> y)
                 .orElseThrow(() -> new NoSuchElementException("The collection is empty"));
     }
 
-    public static int method6(int[] array){
+    public static int getSumOfEvenNum(int[] array){
         return Arrays.stream(array)
                 .filter(x -> x % 2 == 0)
                 .reduce(0, Integer::sum);
     }
 
-    public static Map<Character, String > method7(List<String> list){
+    public static Map<Character, String > convertToMap(List<String> list){
         return list.stream()
                 .collect(Collectors.toMap(s -> s.charAt(0), s -> s.substring(1)));
     }
